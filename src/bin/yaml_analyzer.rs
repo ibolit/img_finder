7f413ea56f07231154ddda71912464c527821e34
Timofey Danshin <t.danshin@gmail.com>
2025-05-02 16:27:16 +0300

    A heavily stripped down version
    Let's do the exif now

---
+++ src/bin/yaml_analyzer.rs
@@ -0,0 +1,178 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs::{self, File},
+    io::Write,
+    path::Path,
+};
+
+use serde::{Deserialize, Serialize};
+
+fn main() {
+    let paths = fs::read_dir("./").unwrap();
+    let mut result: HashMap<String, Vec<Image>> = HashMap::new();
+    for path in paths {
+        let path = path.unwrap();
+
+        if !path
+            .file_name()
+            .to_str()
+            .to_owned()
+            .unwrap()
+            .ends_with("DIRTY_images.yaml")
+        // .ends_with("DIRTY_dups.yaml")
+        {
+            println!("Continuing for path {:?}", path);
+            continue;
+        }
+
+        println!("Processing: {:?}", path);
+
+        let contents = fs::read_to_string(path.path()).unwrap();
+        let ht: HashMap<String, Vec<Image>> = serde_yaml::from_str(&contents).unwrap();
+
+        // for (k, mut v) in ht {
+        //     if !result.contains_key(&k) {
+        //         result.insert(k, v);
+        //         continue;
+        //     }
+        //     result.get_mut(&k).unwrap().append(v.as_mut());
+        // }
+        //
+        //
+        // let dups: Vec<(Image, Vec<Image>)> = serde_yaml::from_str(&contents).unwrap();
+        println!("Moving good files");
+        for (sha, imgs) in ht {
+            // fs::rename(
+            //     imgs[0].path.clone(),
+            //     format!(
+            //         "/Volumes/FlashCard/__UNIQUES__4/{}-{}",
+            //         imgs[0].name,
+            //         urlencoding::encode(&imgs[0].path),
+            //     ),
+            // )
+            // .expect(&format!(
+            //     "Failed to move it move it: {}",
+            //     imgs[0].path.clone()
+            // ));
+            // println!(
+            //     "mv '{}' '/Volumes/Hippie/UNIQUES_1/{}-{}'",
+            //     imgs[0].path,
+            //     imgs[0].name,
+            //     urlencoding::encode(&imgs[0].path),
+            // );
+        }
+    }
+
+    println!("Moving duplicates");
+    let dup_contents = fs::read_to_string("%2FVolumes%2FFlashCard%2FDIRTY_dups.yaml").unwrap();
+
+    let dups: Vec<(Image, Vec<Image>)> = serde_yaml::from_str(&dup_contents).unwrap();
+    for (img, _) in dups {
+        fs::rename(
+            img.path.clone(),
+            format!(
+                "/Volumes/FlashCard/__DUPES__4/{}-{}",
+                img.name,
+                urlencoding::encode(&img.path),
+            ),
+        )
+        .expect(&format!("Failed to move it a dup file: {}", img.path));
+    }
+
+    // let mut imgs_by_folder: HashMap<String, Vec<Image>> = HashMap::new();
+
+    // for (_, imgs) in result {
+    //     let is_duplicate = imgs.len() > 1;
+    //     if !is_duplicate {
+    //         continue;
+    //     }
+
+    //     let mut imgs_iter = imgs.iter();
+    //     let img = imgs_iter.next().unwrap();
+    //     if !fs::exists(&img.path).expect("Exists?") {
+    //         panic!("Original file does not exist");
+    //     }
+    //     println!(": # -- {}", img.path);
+
+    //     for img in imgs_iter {
+    //         if is_duplicate {
+    //             // if !fs::exists(&img.path).expect("Exists 2?") {
+    //             //     continue;
+    //             // }
+
+    //             // fs::rename(
+    //             //     img.path.clone(),
+    //             //     format!(
+    //             //         "/Volumes/Hippie/__DUPLICATES__/{}-{}",
+    //             //         img.name,
+    //             //         urlencoding::encode(&img.path),
+    //             //     ),
+    //             // )
+    //             // .expect("Failed to move it move it");
+    //             println!(
+    //                 "mv '{}' '/Volumes/Hippie/duplicates/{}-{}'",
+    //                 img.path,
+    //                 img.name,
+    //                 urlencoding::encode(&img.path),
+    //             );
+    //         }
+
+    //         // img.is_duplicate = is_duplicate;
+    //         // let p = Path::new(&img.path).parent().unwrap();
+    //         // imgs_by_folder
+    //         //     .entry(p.as_os_str().to_str().unwrap().to_owned())
+    //         //     .or_insert(vec![])
+    //         //     .push(*img);
+    //     }
+    // }
+
+    // let mut dup_folders: Vec<String> = vec![];
+    // for (path, imgs) in imgs_by_folder.clone() {
+    //     let all_dups = imgs.iter().fold(true, |acc, img| acc & img.is_duplicate);
+    //     // dup_folders.insert(path, all_dups);
+    //     if all_dups {
+    //         // println!("{}", path);
+    //         dup_folders.push(path);
+    //     }
+    // }
+
+    // let mut seen_folders: HashSet<String> = HashSet::new();
+
+    // for dup_folder in dup_folders.iter() {
+    //     if seen_folders.contains(dup_folder) {
+    //         continue;
+    //     }
+    // let this_set = shas(&imgs_by_folder[dup_folder].clone());
+    // seen_folders.insert(dup_folder.to_owned());
+
+    // for second_folder in dup_folders.iter() {
+    //     if seen_folders.contains(second_folder) {
+    //         continue;
+    //     }
+    //     let second_set = shas(&imgs_by_folder[second_folder].clone());
+    //     if this_set == second_set {
+    //         println!("{dup_folder} == {second_folder}");
+    //         seen_folders.insert(second_folder.to_owned());
+    //     }
+    // }
+    // }
+
+    // println!("{:?}", dup_folders);
+
+    // let mut out_file = File::create("merged.yaml").unwrap();
+    // let serialized = serde_yaml::to_string(&result).unwrap();
+    // out_file.write_all(serialized.as_bytes()).unwrap();
+}
+
+fn shas(imgs: &Vec<Image>) -> HashSet<String> {
+    imgs.iter().map(|img| img.sha256.clone()).collect()
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Image {
+    path: String,
+    name: String,
+    sha256: String,
+    #[serde(default)]
+    is_duplicate: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use chrono::prelude::*;
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 use sha256;
-use std::{collections::HashMap, fs, path::PathBuf, sync::mpsc::channel};
+use std::io::prelude::*;
+use std::thread::current;
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    path::PathBuf,
+    sync::mpsc::channel,
+};
 use threadpool::ThreadPool;
 use walkdir::WalkDir;
 
 static IMGS: &[&str] = &[
-    "jpeg", "jpg", "heic", "png", "mov", "mp4", "gif", "aae", "tiff",
+    "jpeg", "jpg", "heic", "png", "mov", "mp4", "gif", "aae", "tiff", "wav", "avi", "m4v", "mpg",
+    "mpeg", "tiff", "tif", "raf", "raw",
 ];
 static KNOWN_EXT: &[&str] = &[
     "timestamp",
@@ -25,28 +34,44 @@ struct Args {
     #[arg(short, long)]
     folder: String,
 }
+
 fn main() {
+    let contents = fs::read_to_string("hippie_images.yaml").unwrap();
+    let mut ht: HashMap<String, Vec<Image>> = serde_yaml::from_str(&contents).unwrap();
+
     let pool = ThreadPool::new(4);
     let (img_tx, img_rx) = channel();
     let mut unknowns: HashMap<String, Vec<String>> = HashMap::new();
     let args = Args::parse();
     println!("{:?}", args);
 
-    // let mut imgs = Mutex::new(0);
     let mut imgs = 0;
-    for entry in WalkDir::new(&args.folder) {
+
+    log_time("Before the dir walk");
+    for entry in WalkDir::new(&args.folder)
+        .follow_root_links(false)
+        .follow_links(false)
+        // .max_depth(4)
+        .same_file_system(true)
+    {
+        if let Err(x) = entry {
+            println!("{}", x);
+            continue;
+        }
         let path = entry.unwrap().into_path();
-        // let path = entry.path();
+        if path.is_symlink() {
+            println!("Skipping a symlink at {:?}", path);
+            continue;
+        }
+
         let ext = extension(&path);
         if let Some(ext) = ext {
             if is_image(&ext) {
                 imgs += 1;
                 let img_tx = img_tx.clone();
                 pool.execute(move || {
-                    let data: Vec<u8> = fs::read(&path)
-                        .ok()
-                        .expect(&format!("Failed to read image {:?}", path));
-                    let sha = sha256::digest(data);
+                    let sha = sha256::try_digest(&path)
+                        .expect(&format!("Failed to calculate sha for file {:?}", &path));
                     img_tx
                         .send((
                             path.to_str()
@@ -69,17 +94,160 @@ fn main() {
         }
     }
 
-    println!("After the loop");
+    log_time("After the loop");
 
     let images = img_rx
         .iter()
         .take(imgs)
         .map(|(path, sha)| Image::new(path, sha))
         .collect::<Vec<Image>>();
-    let yaml = serde_yaml::to_string(&images).expect("Da fuck");
-    println!("yaml is: {}", yaml);
+    log_time("Done calculating shas");
+    let mut imgs_by_hash: HashMap<String, Vec<Image>> = HashMap::new();
+    let mut imgs_to_remove: Vec<(Image, Vec<Image>)> = vec![];
 
-    println!("Unknowns are: {:?}", unknowns);
+    for i in images.iter() {
+        if ht.contains_key(&i.sha256) {
+            imgs_to_remove.push((i.clone(), ht[&i.sha256].clone()));
+        } else {
+            ht.insert(i.sha256.clone(), vec![i.clone()]);
+            imgs_by_hash
+                .entry(i.sha256.clone())
+                .or_insert(vec![])
+                .push(i.clone());
+        }
+    }
+    log_time("Done making the hashmap");
+
+    let yaml = serde_yaml::to_string(&imgs_by_hash).expect("Da fuck");
+    log_time("Done serializing");
+    let mut file = File::create(format!("{}_images.yaml", urlencoding::encode(&args.folder)))
+        .expect("Failed to open a file for writing image info");
+    file.write_all(yaml.as_bytes())
+        .expect("Failed to write image info");
+
+    let yaml_new_hippie_imgs = serde_yaml::to_string(&ht).expect("Da fuck");
+    log_time("Done serializing");
+    let mut new_hippie_yaml = fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open("hippie_images.yaml")
+        .expect("Failed to open a file for writing image info");
+    new_hippie_yaml
+        .write_all(yaml_new_hippie_imgs.as_bytes())
+        .expect("Failed to write image info");
+
+    let mut unexp_file = File::create(format!("{}_unexp.yaml", urlencoding::encode(&args.folder)))
+        .expect("Failed to create a file for writing unexpected things");
+    let unknowns_yaml = serde_yaml::to_string(&unknowns).expect("Convert unknowns to yaml");
+    unexp_file
+        .write_all(unknowns_yaml.as_bytes())
+        .expect("failed to write unknonwn");
+
+    let mut dups_file = File::create(format!("{}_dups.yaml", urlencoding::encode(&args.folder)))
+        .expect("Failed to create a file for writing unexpected things");
+    let unknowns_yaml = serde_yaml::to_string(&imgs_to_remove).expect("Convert to_remove to yaml");
+    dups_file
+        .write_all(unknowns_yaml.as_bytes())
+        .expect("failed to write unknonwn");
+}
+
+fn main_2() {
+    let pool = ThreadPool::new(4);
+    let (img_tx, img_rx) = channel();
+    let mut unknowns: HashMap<String, Vec<String>> = HashMap::new();
+    let args = Args::parse();
+    println!("{:?}", args);
+
+    let mut imgs = 0;
+
+    log_time("Before the dir walk");
+    for entry in WalkDir::new(&args.folder)
+        .follow_root_links(false)
+        .follow_links(false)
+        .max_depth(4)
+        .same_file_system(true)
+    {
+        if let Err(x) = entry {
+            println!("{}", x);
+            continue;
+        }
+        let path = entry.unwrap().into_path();
+        if path.is_symlink() {
+            println!("Skipping a symlink at {:?}", path);
+            continue;
+        }
+        let curr_dir = path.file_name().unwrap().to_str().unwrap();
+
+        if curr_dir == "System"
+            || curr_dir == "Users"
+            || curr_dir == "etc"
+            || curr_dir == "private"
+            || curr_dir == "usr"
+        {
+            println!("Got {}", curr_dir);
+            continue;
+        }
+
+        let ext = extension(&path);
+        if let Some(ext) = ext {
+            if is_image(&ext) {
+                imgs += 1;
+                let img_tx = img_tx.clone();
+                pool.execute(move || {
+                    let sha = sha256::try_digest(&path)
+                        .expect(&format!("Failed to calculate sha for file {:?}", &path));
+                    img_tx
+                        .send((
+                            path.to_str()
+                                .expect(&format!("Path has no str, {:?}", path))
+                                .to_owned(),
+                            sha,
+                        ))
+                        .expect("Chan must not be closed");
+                });
+            } else if !is_known(&ext) {
+                let str_path = path
+                    .to_str()
+                    .expect(&format!("path has no str {:?}", path))
+                    .to_owned();
+                unknowns
+                    .entry(ext)
+                    .and_modify(|paths| paths.push(str_path.to_owned()))
+                    .or_insert(vec![str_path]);
+            }
+        }
+    }
+
+    log_time("After the loop");
+
+    let images = img_rx
+        .iter()
+        .take(imgs)
+        .map(|(path, sha)| Image::new(path, sha))
+        .collect::<Vec<Image>>();
+    log_time("Done calculating shas");
+    let mut imgs_by_hash: HashMap<String, Vec<Image>> = HashMap::new();
+    for i in images {
+        imgs_by_hash
+            .entry(i.sha256.clone())
+            .or_insert(vec![])
+            .push(i);
+    }
+    log_time("Done making the hashmap");
+
+    let yaml = serde_yaml::to_string(&imgs_by_hash).expect("Da fuck");
+    log_time("Done serializing");
+    let mut file = File::create(format!("{}_images.yaml", urlencoding::encode(&args.folder)))
+        .expect("Failed to open a file for writing image info");
+    file.write_all(yaml.as_bytes())
+        .expect("Failed to write image info");
+
+    let mut unexp_file = File::create(format!("{}_unexp.yaml", urlencoding::encode(&args.folder)))
+        .expect("Failed to create a file for writing unexpected things");
+    let unknowns_yaml = serde_yaml::to_string(&unknowns).expect("Convert unknowns to yaml");
+    unexp_file
+        .write_all(unknowns_yaml.as_bytes())
+        .expect("failed to write unknonwn");
 }
 
 fn extension(entry: &PathBuf) -> Option<String> {
@@ -94,7 +262,7 @@ fn is_known(ext: &str) -> bool {
     KNOWN_EXT.contains(&ext)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Image {
     path: String,
     name: String,
@@ -112,4 +280,8 @@ impl Image {
         };
         Self { path, name, sha256 }
     }
+}
+
+fn log_time(msg: &str) {
+    println!("{}: {}", Utc::now().to_string(), msg);
 }
